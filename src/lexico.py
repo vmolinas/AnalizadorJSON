d9931a545827dd67a00ca9643cc8be541148320d
Victor Molinas <u.vmolinas@gmail.com>
2024-05-30 21:58:18 -0300

    Mensajes personalizados para lexer

--- src/lexico.py
+++ src/lexico.py
@@ -10,28 +10,6 @@ tokens = ("NEWLINE", "BLANK", "COMA", "CORCHETEA", "CORCHETEC", "LLAVEA", "LLAVE
           "FECHA_CONTRATACION", "PROYECTOS", "ESTADO", "FECHA_INICIO", "FECHA_FIN",
           "FLOAT", "INTEGER", "FECCHA", "STRING")
 
-# Mensaje predeterminado para los elementos reconocidos
-
-def imprimirMensajeKey(t):
-    print(t.lineno, " KEY: " + t.value, end="")
-
-def imprimirMensajeKeyNV(t):
-    print(t.lineno, " KEY: " + t.value)
-
-def imprimirMensajeValor(t):
-    print(t.lineno, " VALOR: " + t.value)
-    # print(" VALOR: " + t.value)
-
-def imprimirError(t):
-    print("Token no reconocido: ", t.value + " [Linea:" , t.lineno,"]")
-
-def imprimirApertura(t):
-    print(t.lineno, " APERTURA: " + t.value)
-    
-def imprimirCierre(t):
-    print(t.lineno, " CIERRE: " + t.value)
-
-
 def t_NEWLINE(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
@@ -44,159 +22,159 @@ def t_COMA(t):
 
 def t_CORCHETEA(t):
     r'\['
-    imprimirApertura(t)
+    print(t.lineno, " APERTURA CORCHETE: " + t.value)
 
 def t_CORCHETEC(t):
     r'\]'
-    imprimirCierre(t)
+    print(t.lineno, " CIERRE CORCHETE: " + t.value)
 
 def t_LLAVEA(t):
     r'\{'
-    imprimirApertura(t)
+    print(t.lineno, " APERTURA LLAVE: " + t.value)
 
 def t_LLAVEC(t):
     r'\}'
-    imprimirCierre(t)
+    print(t.lineno, " CIERRE LLAVE: " + t.value)
 
 def t_DPUNTOS(t):
     r'\:'
 
 def t_NULL(t):
     r'null'
-    imprimirMensajeValor(t)
+    print(t.lineno, " VALOR: " + t.value)
 
 def t_FALSE(t):
     r'false'
-    imprimirMensajeValor(t)
+    print(t.lineno, " BOOLEANO: " + t.value)
 
 def t_TRUE(t):
     r'true'
-    imprimirMensajeValor(t)
+    print(t.lineno, " BOOLEANO: " + t.value)
 
 def t_URI(t):
     r'"(http|https|ftp)://[^\s<>&]+(\.[^\s<>&]+)*(:\d+)?(/[^\s<>&]*)*(\?[^\s<>&]*)?(\#[^\s<>&]*)?"'
-    imprimirMensajeValor(t)
+    print(t.lineno, " URL: " + t.value)
 
 def t_EMPRESAS(t):
     r'"empresas"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY EMPRESAS: " + t.value)
 
 def t_VERSION(t):
     r'"version"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY VERSION: " + t.value)
 
 def t_FIRMA_DIGITAL(t):
     r'"firma_digital"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY FIRMA DIGITAL: " + t.value)
 
 def t_NOMBRE_EMPRESA(t):
     r'"nombre_empresa"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY NOMBRE EMPRESA: " + t.value)
 
 def t_FUNDACION(t):
     r'"fundación"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY FUNDACION: " + t.value)
 
 def t_DIRECCION(t):
     r'"dirección"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY DIRECCION: " + t.value)
 
 def t_INGRESOS_ANUALES(t):
     r'"ingresos_anuales"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY INGRESOS ANUALES: " + t.value)
 
 def t_PYME(t):
     r'"pyme"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY PYME: " + t.value)
 
 def t_LINK(t):
     r'"link"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY LINK: " + t.value)
 
 def t_DEPARTAMENTOS(t):
     r'"departamentos"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY DEPARTAMENTOS: " + t.value)
 
 def t_CALLE(t):
     r'"calle"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY CALLE: " + t.value)
 
 def t_CIUDAD(t):
     r'"ciudad"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY CIUDAD: " + t.value)
 
 def t_PAIS(t):
     r'"país"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY PAIS: " + t.value)
 
 def t_NOMBRE(t):
     r'"nombre"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY NOMBRE: " + t.value)
 
 def t_JEFE(t):
     r'"jefe"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY JEFE: " + t.value)
 
 def t_SUBDEPARTAMENTOS(t):
     r'"subdepartamentos"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY SUBDEPARTAMENTOS: " + t.value)
 
 def t_EMPLEADOS(t):
     r'"empleados"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY EMPLEADOS: " + t.value)
 
 def t_EDAD(t):
     r'"edad"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY EDAD: " + t.value)
 
 def t_CARGO(t):
     r'"cargo"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY CARGO: " + t.value)
 
 def t_SALARIO(t):
     r'"salario"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY SALARIO: " + t.value)
 
 def t_ACTIVO(t):
     r'"activo"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY ACTIVO: " + t.value)
 
 def t_FECHA_CONTRATACION(t):
     r'"fecha_contratación"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY FECHA CONTRATACION: " + t.value)
 
 def t_PROYECTOS(t):
     r'"proyectos"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY PROYECTOS: " + t.value)
 
 def t_ESTADO(t):
     r'"estado"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY ESTADO: " + t.value)
 
 def t_FECHA_INICIO(t):
     r'"fecha_inicio"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY FECHA INICIO: " + t.value)
 
 def t_FECHA_FIN(t):
     r'"fecha_fin"'
-    imprimirMensajeKeyNV(t)
+    print(t.lineno, " KEY FECHA FIN: " + t.value)
 
 def t_FLOAT(t):
     r'[0-9]+\.[0-9]+'
-    imprimirMensajeValor(t)
+    print(t.lineno, " FLOAT: " + t.value)
 
 def t_INTEGER(t):
     r'[0-9]+'
-    imprimirMensajeValor(t)
+    print(t.lineno, " INTEGER: " + t.value)
 
 def t_FECHA(t):
-    r'"\d{4}-\d{2}-\d{2}"'
-    imprimirMensajeValor(t)
+    r'"((19[0-9]{2}|20[0-9]{2})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))"'
+    print(t.lineno, " FECHA: " + t.value)
 
 def t_STRING(t):
     r'"[a-zA-Z0-9 | _ | \- | , | . | : | \{ | \} | \[ | \] | \t]+"'
-    imprimirMensajeValor(t)
+    print(t.lineno, " STRING: " + t.value)
 
 def t_error(t):
-    imprimirError(t)
+    print("TOKEN NO RECONOCIDO: ", t.value + " [Linea:", t.lineno, "]")
     t.lexer.skip(1)
